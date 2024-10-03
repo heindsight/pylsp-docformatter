6ff9b83a854b7bcf7bdafece7548c6ef987a77da
Heinrich Kruger <heindsight@kruger.dev>
2024-10-03 20:53:09 +0100

    Fix context manager typing

--- pylsp_docformatter/util.py
+++ pylsp_docformatter/util.py
@@ -2,11 +2,11 @@
 import contextlib
 import os
 import pathlib
-from typing import Iterator
+from typing import Generator
 
 
 @contextlib.contextmanager
-def temp_work_dir(workdir: pathlib.Path) -> Iterator[None]:
+def temp_work_dir(workdir: pathlib.Path) -> Generator[None, None, None]:
     """Temporarily change working directory."""
     initial_workdir = os.getcwd()
     os.chdir(workdir)
